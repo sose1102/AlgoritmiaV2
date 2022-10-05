df55cc520b47eaa9101f51cd35ffc6d9eea9e240
sose1102 <sose1102@gmail.com>
2022-10-05 16:15:30 +0200

    Carpeta para oscar

---
+++ Jose/Pracicas/Problemas/shortest_path.py
@@ -0,0 +1,7 @@
+from algoritmia.datastructures.graphs import *
+
+
+def bf_search(g: UndirectedGraph[Vertex], source: Vertex, target: Vertex)  -> list[Edge]:
+
+
+def path_recover(edges: list[Vertex]):
